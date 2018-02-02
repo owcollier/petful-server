2469b56de0f30dabc7d17015d7e8cd5e1008018c
Owcollier <owcollier@gmail.com>
2018-02-02 15:19:40 -0500

    added delete endpoints, tested

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ const cats = [
     age: 2,
     breed: 'Bengal',
     story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Mittens',
+    sex: 'Male',
+    age: 4,
+    breed: 'Bengal',
+    story: 'Owner lost in a wormhole incident'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Moon',
+    sex: 'Female',
+    age: 1,
+    breed: 'Bengal',
+    story: 'Brought to shelter'
   }
 ];
 
@@ -31,7 +49,25 @@ const dogs = [
     age: 3,
     breed: 'Golden Retriever',
     story: 'Owner Passed away'
-  } 
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Donovan',
+    sex: 'Male',
+    age: 6,
+    breed: 'Golden Retriever',
+    story: 'Family moved'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Barbara',
+    sex: 'Female',
+    age: 1,
+    breed: 'Golden Retriever',
+    story: 'Found wandering the street'
+  }
 ];
 
 app.use(
@@ -53,6 +89,18 @@ app.get('/api/cat', (req, res) => {
 
 app.get('/api/dog', (req, res) => {
   return res.json(dogs[0]);
+});
+
+app.delete('/api/cat', (req, res) => {
+  const adopted = cats[0];
+  cats.splice(0, 1);
+  return res.json(`${adopted.name} successfully adopted!`);
+});
+
+app.delete('/api/dog', (req, res) => {
+  const adopted = dogs[0];
+  dogs.splice(0, 1);
+  return res.json(`${adopted.name} successfully adopted!`);
 });
 
 function runServer(port = PORT) {
